bea1a2592df5bf85540292e0928b916e68cb497f
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-12-05 09:41:45 -0800

    Add header component
    Co-Authored-By: Ravin Fisher <ravesterrr@gmail.com>
    Co-Authored-By: Grant Abel <access.granted.910@gmail.com>

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import React from "react";
+import Header from "./Header";
 
 function App() {
 	return (
-    
+			<Header />
 );
 }
 
---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <React.Fragment>
+        <h1> Virgil's Vegetables </h1>
+        <h2> The freshest veggies available year-round </h2>
+        </React.Fragment> 
+    )
+}
+
+export default Header;
